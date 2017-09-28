b07a14fa65203a141053e09324fa95745ea2a78c
suqingfa <enmoton@qq.com>
2017-09-28 13:48:33 +0800

    完成学生花名册下载

--- Server/Pages/Teacher/Index.cshtml.cs
+++ Server/Pages/Teacher/Index.cshtml.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 using Server.Data;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -57,6 +59,50 @@ namespace Server.Pages.Teacher
             Course = result.ToArray();
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnGetGetStudentListAsync(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                OnGet();
+                return Page();
+            }
+
+            var studentList = _context.StudentCourses
+                .Include(x => x.Student)
+                .Include(x => x.Student.Profession)
+                .Where(x => x.CourseId == id);
+
+            MemoryStream stream = new MemoryStream();
+
+            using (ExcelPackage package = new ExcelPackage(stream))
+            {
+                // add a new worksheet to the empty workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("学生花名册");
+                //First add the headers
+                worksheet.Cells[1, 1].Value = "专业";
+                worksheet.Cells[1, 2].Value = "学号";
+                worksheet.Cells[1, 3].Value = "姓名";
+                worksheet.Cells[1, 4].Value = "入学时间";
+
+                int i = 1;
+                await studentList.ForEachAsync(x =>
+                {
+                    i++;
+                    worksheet.Cells["A" + i].Value = x.Student.Profession.Name;
+                    worksheet.Cells["B" + i].Value = x.Student.Id;
+                    worksheet.Cells["C" + i].Value = x.Student.Name;
+                    worksheet.Cells["D" + i].Value = x.Student.Admission;
+                });
+
+                package.Save(); //Save the workbook.
+            }
+
+            await stream.FlushAsync();
+            stream.Position = 0;
+
+            return File(stream.GetBuffer(), "application/vnd.ms-excel", "student.xlsx");
         }
     }
 }
